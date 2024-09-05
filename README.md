4e16bc1b799330e47a507630120779f480687c2e
Yeah Jack <95103974+Yeah-Jack@users.noreply.github.com>
2024-09-05 21:39:20 +0200

    Better Styling

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 [![CodeFactor](https://www.codefactor.io/repository/github/yeah-jack/simple-drag-and-drop-website/badge)](https://www.codefactor.io/repository/github/yeah-jack/simple-drag-and-drop-website)
 
 # Simple Drag & Drop Website
+
 Written in HTML, CSS, and Javascript
--- script.js
+++ script.js
@@ -4,19 +4,19 @@ const imageView = document.getElementById("img-view");
 
 inputFile.addEventListener("change", uploadImage);
 
-function uploadImage(){
-    let imgLink = URL.createObjectURL(inputFile.files[0]);
-    imageView.style.backgroundImage = `url(${imgLink})`;
-    imageView.textContent = "";
-    imageView.style.border = 0;
+function uploadImage() {
+	let imgLink = URL.createObjectURL(inputFile.files[0]);
+	imageView.style.backgroundImage = `url(${imgLink})`;
+	imageView.textContent = "";
+	imageView.style.border = 0;
 }
 
-dropArea.addEventListener("dragover", function(e){
-    e.preventDefault();
+dropArea.addEventListener("dragover", function (e) {
+	e.preventDefault();
 });
 
-dropArea.addEventListener("drop", function(e){
-    e.preventDefault();
-    inputFile.files = e.dataTransfer.files;
-    uploadImage();
+dropArea.addEventListener("drop", function (e) {
+	e.preventDefault();
+	inputFile.files = e.dataTransfer.files;
+	uploadImage();
 });
